a6a5a826e40a81f9505aa5bc0822b2cf1185a5d2
book-731 <C112118214@nkust.edu.tw>
2026-02-07 01:21:09 +0800

    fix:暫停時設定選單沒顯示修復

--- Scripts/UI/pause_menu.gd
+++ Scripts/UI/pause_menu.gd
@@ -1,26 +1,28 @@
 extends Control
 
 @onready var setting_menu = $SettingsMenu
+@onready var pause_menu = $VBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#暫停選單打開時，先把設定選單藏起來
 	setting_menu.hide()
 	#顯示暫停選單
-	self.hide()
+	pause_menu.show()
 	#連接設定關閉的訊號
 	setting_menu.finished_settings.connect(_on_setting_menu_closed)
 	pass 
 
 #接收到設定關閉時回到Pause_Menu
 func _on_setting_menu_closed() -> void:
-	self.show()
+	pause_menu.show()
 	pass
 	
 #當設定按鈕被點選時
 func _on_settings_pressed() -> void:
-	self.hide()
+	pause_menu.hide()
 	setting_menu.show()
+	print("進入設定選單")
 	pass
 
 #當Title按鈕被點選時
