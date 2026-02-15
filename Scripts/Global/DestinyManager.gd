aeca568af04dadf3bee6fbad7a7e4aaad00e706d
book-731 <C112118214@nkust.edu.tw>
2026-02-15 23:32:56 +0800

    feat:實作商店葉頁面抽事件功能，不完整。

--- Scripts/Global/DestinyManager.gd
+++ Scripts/Global/DestinyManager.gd
@@ -1,5 +1,12 @@
 extends Node
 
+# 命運管理器，負責生成和應用命運卡的效果
+
+# 敵人傷害和生命的修改器，初始值為0，根據選擇的命運卡進行增減
+var enemy_damage_modifier = 0
+var enemy_health_modifier = 0
+var enemy_walk_speed_modifier = 0
+var enemy_quantity_multiplier = 0
 var destiny_data = {
     "Enemy_damage_increase": {
         "title": "黑暗詛咒",
@@ -31,6 +38,17 @@ var destiny_data = {
         "icon": "res://path_to_destiny_6_icon.png",
         "description": "敵人的生命減少20"
     },
+    "Enemy_speed_increase": {
+        "title": "暗影的加速",
+        "icon": "res://path_to_destiny_7_icon.png",
+        "description": "敵人的移動速度增加20"
+    },
+    "Enemy_quantity_increase": {
+        "title": "怪物奉上",
+        "icon": "res://path_to_destiny_8_icon.png",
+        "description": "敵人數量增加10%"
+    },
+
 }
 
 func destiny_random() -> Dictionary:
@@ -39,22 +57,29 @@ func destiny_random() -> Dictionary:
     return destiny_data[random_key]
 
 func destiny_apply(destiny: Dictionary) -> void:
+    print("Applying destiny: " + destiny["title"])
     match destiny["title"]:
         "黑暗詛咒":
             # 增加敵人傷害20
-            pass
+            enemy_damage_modifier += 20
         "惡魔的詠唱":
             # 增加敵人生命20
-            pass
+            enemy_health_modifier += 20
         "戰神的祝福":
             # 增加玩家傷害20
-            pass
+            PlayerData.player_damage += 20
         "女神的庇護":
             # 增加玩家生命20
-            pass
+            PlayerData.player_max_health += 20
         "光明的庇護":
             # 減少敵人傷害20
-            pass
+            enemy_damage_modifier -= 20
         "光明的枷鎖":
             # 減少敵人生命20
-            pass
+            enemy_health_modifier -= 20
+        "暗影的加速":
+            # 增加敵人移動速度20
+            enemy_walk_speed_modifier += 20
+        "怪物奉上":
+            # 增加敵人數量10%
+            enemy_quantity_multiplier += 0.1
--- Scripts/UI/new_shop_menu.gd
+++ Scripts/UI/new_shop_menu.gd
@@ -1,5 +1,10 @@
 extends Control
 
+# 商店菜單，顯示三張隨機命運卡，玩家可以選擇其中一張來應用其效果
+
+#暫存當前顯示的命運卡數據
+var current_card : Array = []
+
 func _ready() -> void:
 	#進入商店時隨機生成3張命運卡
 	for i in range(3):
@@ -8,19 +13,22 @@ func _ready() -> void:
 		var card = $HBoxContainer.get_node("Card" + str(i + 1))
 		card.get_node("VBoxContainer/Title").text = destiny.title
 		card.get_node("VBoxContainer/ScrollContainer/Description").text = destiny.description
+		current_card.append(destiny)
 	pass
 
 
 
 
 func _on_card_1_pressed() -> void:
-	
+	DestinyManager.destiny_apply(current_card[0])
 	pass # Replace with function body.
 
 
 func _on_card_2_pressed() -> void:
+	DestinyManager.destiny_apply(current_card[1])
 	pass # Replace with function body.
 
 
 func _on_card_3_pressed() -> void:
+	DestinyManager.destiny_apply(current_card[2])
 	pass # Replace with function body.
